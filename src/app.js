06687381811d7355d93b28808234d70b1d5c4201
chitransh100 <kumarchitransh12@gmail.com>
2025-01-10 23:32:01 +0530

    exploring the http methods

--- src/app.js
+++ src/app.js
@@ -1,22 +1,31 @@
-// Importing the Express library
 const express = require("express");
-//  initialize an instance of an Express application
 const app = express();
-// Defining the port number for the server
-const PORT = 7777; // Use a number for the port
-// Starting the server and listening on the specified port
+const PORT = 7777; 
 app.listen(PORT, (error) => {
     if (!error) {
-        // If no error, the server starts successfully
         console.log(`Server is listening on port ${PORT}`);
     } else {
-        // If there is an error starting the server, it is logged to the console
         console.error("Error in starting the server:", error);
     }
 });
-// Setting up a route for the root URL ("/")
-app.use("/", (req, res) => {
-    // Responds with "Hello world" when a request is made to the root URL
-    res.send("Hello world");
-});
-
+// app.use("/", (req, res) => {
+//     res.send("Hello world");
+// });
+// app.use("/hello",(req,res)=>{
+//     res.send("hello ");
+// })
+// app.use("/test",(req,res)=>{
+//     res.send("hello from the server");
+// })
+app.use("/user",(req,res)=>{
+    res.send("i will never give chance to other")
+})
+app.get("/user",(req,res)=>{
+    res.send({name:"chitransh",age:"22"})
+})
+app.post("/user",(req,res)=>{
+    res.send("data saved to database")
+})
+app.delete("/user",(req,res)=>{
+    res.send("deleted !!")
+})
